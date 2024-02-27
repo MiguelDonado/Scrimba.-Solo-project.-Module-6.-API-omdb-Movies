a0e294253ef6def6878da697fa893ee16c1d8bd0
MiguelDonado <miguel.5799@hotmail.es>
2024-02-27 22:07:31 +0100

    Finish basic and stretch goals. Yet to be done order movies by number of votes on index.html

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const searchInputEl = document.getElementById('search-input')
-const searchBtnEl = document.getElementById('search-btn')
-const magnifyingBtnEl = document.getElementById('magnifying-class-btn')
 const afterSearchContainer = document.getElementById('after-search-container')
 const beforeSearchContainer = document.getElementById('before-search-container')
 const unableFindContainer = document.getElementById('unable-find-container')
@@ -13,28 +11,51 @@ searchInputEl.addEventListener('keydown', function (e){
     }
 })
 
-searchBtnEl.addEventListener('click', function () {
-    loadJsonSearch()
-})
-
-magnifyingBtnEl.addEventListener('click', function () {
-    loadJsonSearch()
-})
-
 document.addEventListener('click', function (e){
     if (e.target.dataset.add) {
         myWatchlist.push(e.target.dataset.add)
+        alert('A movie has been added to your watchlist.')
     }else if (e.target.id === "header-anchor") {
         e.preventDefault()
-        localStorage.setItem('myWatchlist',JSON.stringify(myWatchlist))
+        moviesAlreadyInLocalStorage = JSON.parse(localStorage.getItem('myWatchlist'))
+        listMoviesToSet = convertToSet([...moviesAlreadyInLocalStorage, ...myWatchlist])
+        localStorage.setItem('myWatchlist',JSON.stringify(listMoviesToSet))
         window.location.href = e.target.href
+    }else if (e.target.id === "search-btn" || e.target.id === "magnifying-class-btn" ) {
+        loadJsonSearch()
     }
 })
+
+function convertToSet(array) {
+    let newArrayAsSet = []
+    array.forEach(item => {
+        if (!newArrayAsSet.includes(item)){
+            newArrayAsSet.push(item)
+        }
+    })
+    return newArrayAsSet
+}
+
+function loadJsonSearch() {
+    beforeSearchContainer.style.display = 'none'
+    unableFindContainer.style.display = 'none'
+    afterSearchContainer.style.display = 'flex'
+    afterSearchContainer.innerHTML = ``
+    moviesHtml = `` 
+    return fetch(`http://www.omdbapi.com/?apikey=858c0a0a&s=${searchInputEl.value}&type=movie`)
+    .then(response => response.json())
+    .then(data => getImdbIdsSearch(data))
+    .then(ids => renderMovies(ids))
+    .catch(error => {
+        unableFindContainer.style.display = 'flex'
+        afterSearchContainer.style.display = 'none'
+    })
+    .finally(() => searchInputEl.value = '')
+}
 
 function getImdbIdsSearch (arraySearch) {
     return arraySearch.Search.map(item => item.imdbID)
 }
-
 function renderMovies (moviesId) {
     return Promise.all(moviesId.map(renderMovie)).then(() => {
         afterSearchContainer.innerHTML = moviesHtml;
@@ -45,13 +66,13 @@ function renderMovie (movieId) {
     return fetch(`http://www.omdbapi.com/?apikey=858c0a0a&i=${movieId}`)
         .then(response => response.json())
         .then(data => {
-        moviesHtml+= `
+            moviesHtml+= `
         <div class="movie-container">
             <div class="img-movie-container">
                 <img class="img-movie" src="${data.Poster}">
             </div>
             <div class="inner-movie-container">
-                <div class="top-inner-movie">
+            <div class="top-inner-movie">
                     <h1 class="title">${data.Title}</h1>
                     <p class="stars">⭐ ${data.Ratings[0].Value}</p>
                 </div>
@@ -59,33 +80,16 @@ function renderMovie (movieId) {
                     <p class="duration">${data.Runtime}</p>
                     <p class="topics">${data.Genre}</p>
                     <div class="add-watchlist">
-                        <i class="fa-solid fa-circle-plus" data-add=${movieId}></i>
-                        <p class="add-text">Watchlist</p>
+                    <i class="fa-solid fa-circle-plus" data-add=${movieId}></i>
+                    <p class="add-text">Watchlist</p>
                     </div>
                 </div>
                 <div class="bottom-inner-movie">
                     <p class="description">${data.Plot}</p>
-                </div>
-            </div>
-        </div>
+                    </div>
+                    </div>
+                    </div>
         `
     })
 }
-
-function loadJsonSearch() {
-    beforeSearchContainer.style.display = 'none'
-    unableFindContainer.style.display = 'none'
-    afterSearchContainer.style.display = 'flex'
-    afterSearchContainer.innerHTML = ``
-    moviesHtml = `` 
-    return fetch(`http://www.omdbapi.com/?apikey=858c0a0a&s=${searchInputEl.value}&type=movie`)
-        .then(response => response.json())
-        .then(data => getImdbIdsSearch(data))
-        .then(ids => renderMovies(ids))
-        .catch(error => {
-            unableFindContainer.style.display = 'flex'
-            afterSearchContainer.style.display = 'none'
-    })
-}
-
 
---
+++ myWatchlist.js
@@ -0,0 +1,63 @@
+const moviesListContainer = document.getElementById('movies-list-container')
+const emptyListContainer = document.getElementById('empty-list-container')
+myWatchlist = []
+moviesHtml = ``
+
+document.addEventListener('click', function (e){
+    if (e.target.dataset.minus) {
+        myWatchlist = myWatchlist.filter(item => item!==e.target.dataset.minus)
+        localStorage.setItem('myWatchlist',JSON.stringify(myWatchlist))
+        alert('A movie has been removed from your watchlist.')
+        renderListMovies()
+    }})
+
+function renderMovies (moviesId) {
+    return Promise.all(moviesId.map(renderMovie)).then(() => {
+        moviesListContainer.innerHTML = moviesHtml;
+    });
+}
+
+function renderMovie (movieId) {
+    return fetch(`http://www.omdbapi.com/?apikey=858c0a0a&i=${movieId}`)
+        .then(response => response.json())
+        .then(data => {
+        moviesHtml+= `
+        <div class="movie-container">
+            <div class="img-movie-container">
+                <img class="img-movie" src="${data.Poster}">
+            </div>
+            <div class="inner-movie-container">
+                <div class="top-inner-movie">
+                    <h1 class="title">${data.Title}</h1>
+                    <p class="stars">⭐ ${data.Ratings[0].Value}</p>
+                </div>
+                <div class="center-inner-movie">
+                    <p class="duration">${data.Runtime}</p>
+                    <p class="topics">${data.Genre}</p>
+                    <div class="remove-watchlist">
+                        <i class="fa-solid fa-circle-minus" data-minus=${movieId}></i>
+                        <p class="remove-text">Watchlist</p>
+                    </div>
+                </div>
+                <div class="bottom-inner-movie">
+                    <p class="description">${data.Plot}</p>
+                </div>
+            </div>
+        </div>
+        `
+    })
+}
+
+function renderListMovies() {
+    moviesID = JSON.parse(localStorage.getItem('myWatchlist'))
+    myWatchlist = moviesID
+    moviesHtml = `` 
+    if (myWatchlist.length) {
+        emptyListContainer.style.display = 'none'
+        moviesListContainer.style.display = 'flex'
+        renderMovies(moviesID)
+    }
+}
+
+
+renderListMovies()
